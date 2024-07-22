8128db67a921ee669c17ac937e2dde510ec3449b
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-22 07:39:11 +0200

    inital commit

---
+++ src/main/java/org/example/groupbackend/GroupBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.groupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/groupbackend/GroupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
