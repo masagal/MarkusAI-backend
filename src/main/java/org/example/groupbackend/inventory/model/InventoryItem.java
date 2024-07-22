6841f9620c1f38a237996c618a96de339724f435
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 13:49:34 +0200

    create new items in the inventory

--- src/main/java/org/example/groupbackend/inventory/model/InventoryItem.java
+++ src/main/java/org/example/groupbackend/inventory/model/InventoryItem.java
@@ -11,7 +11,7 @@ public class InventoryItem {
     Logger logger = LogManager.getLogger();
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     String id;
 
     String label;
