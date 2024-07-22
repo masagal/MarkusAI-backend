cbb345ce62e1fc74476fb18fe0b96058d4c39dcf
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 13:05:25 +0200

    move integration test out of http package

--- src/test/java/org/example/groupbackend/inventory/InventoryIntegrationTest.java
+++ src/test/java/org/example/groupbackend/inventory/InventoryIntegrationTest.java
@@ -1,10 +1,9 @@
-package org.example.groupbackend.inventory.http;
+package org.example.groupbackend.inventory;
 
+import org.example.groupbackend.inventory.http.InventoryController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,13 +11,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
