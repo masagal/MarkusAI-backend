1de36bcde84dfbb58f4dc696edb381a091fbaa3f
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 14:21:39 +0200

    use products in service and items

--- src/main/java/org/example/groupbackend/products/Product.java
+++ src/main/java/org/example/groupbackend/products/Product.java
@@ -1,11 +1,9 @@
 package org.example.groupbackend.products;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
