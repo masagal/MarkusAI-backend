b306630b92d46664d40cf2a8cb671ab62b4b58a8
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-22 16:48:56 +0200

    some test data

---
+++ src/main/resources/data-mathangi.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_test_class (name) VALUES ('testUser');
+INSERT INTO request (user_id) VALUES (1);
+INSERT INTO request (user_id) VALUES (1);
+INSERT INTO request_product (product_id, request_id, quantity) VALUES (1, 1, 10);
+INSERT INTO request_product (product_id, request_id, quantity) VALUES (2, 2, 5);
