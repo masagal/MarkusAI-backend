895e1590cf46561e41d60c0a082ccf61579e56b4
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-22 13:15:14 +0200

    endpoint for posting new request

--- src/main/java/org/example/groupbackend/request/RequestController.java
+++ src/main/java/org/example/groupbackend/request/RequestController.java
@@ -1,4 +1,26 @@
 package org.example.groupbackend.request;
 
+import org.example.groupbackend.request.classesForTesting.UserTestClass;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/requests")
 public class RequestController {
+
+    private final RequestService requestService;
+
+    public RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    /*@GetMapping
+    private ResponseEntity<Request> getAllRequests() {
+    }*/
+
+    @PostMapping
+    private ResponseEntity<Request> postNewRequest(@RequestBody RequestDto requestDto) {
+        var postRequest = requestService.newRequest(new Request(requestDto.quantity()), requestDto.userId());
+        return ResponseEntity.ok(postRequest);
+    }
 }
