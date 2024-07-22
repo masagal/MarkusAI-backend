da0323fa0facf52de84ae3f06e52b75e6e8e83f7
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 13:49:34 +0200

    create new items in the inventory

--- src/main/java/org/example/groupbackend/inventory/http/dto/InventoryItemDto.java
+++ src/main/java/org/example/groupbackend/inventory/http/dto/InventoryItemDto.java
@@ -2,8 +2,8 @@ package org.example.groupbackend.inventory.http.dto;
 
 import org.example.groupbackend.inventory.model.InventoryItem;
 
-public record InventoryItemDto(String label, String quantity) {
+public record InventoryItemDto(String id, String label, String quantity) {
     public static InventoryItemDto getDto(InventoryItem item) {
-        return new InventoryItemDto(item.getLabel(), item.getQuantity().toString());
+        return new InventoryItemDto(item.getId(), item.getLabel(), item.getQuantity().toString());
     }
 }
--- src/test/java/org/example/groupbackend/inventory/InventoryIntegrationTest.java
+++ src/test/java/org/example/groupbackend/inventory/InventoryIntegrationTest.java
@@ -4,6 +4,8 @@ import org.example.groupbackend.inventory.http.InventoryController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,11 +15,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -56,5 +56,24 @@ class InventoryIntegrationTest {
         mockMvc.perform(get)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[2].quantity", is("10")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"""
+                {"label": "Grue whiteboard marker"}
+                """, """
+                {"label": "Purple whiteboard marker", "quantity": "4"}
+                """})
+    public void shouldCreateNewItemType(String body) throws Exception{
+
+        var post = MockMvcRequestBuilders.post(InventoryController.ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body);
+
+        mockMvc.perform(post)
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("location"))
+                .andExpect(jsonPath("$.id", not(is(empty()))));
+
     }
 }
