84663e06d3856832619d0193602c9abca1811931
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 15:49:45 +0200

    clean up

--- src/main/java/org/example/groupbackend/inventory/http/dto/UpdateInventoryItemDto.java
+++ src/main/java/org/example/groupbackend/inventory/http/dto/UpdateInventoryItemDto.java
@@ -2,5 +2,5 @@ package org.example.groupbackend.inventory.http.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record UpdateInventoryItemDto(String id, String label, @JsonProperty(value = "quantity") Integer quantity) {
+public record UpdateInventoryItemDto(String id, String label, Integer quantity) {
 }
