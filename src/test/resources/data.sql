4648c24caa83555fb9decd36648b0eaf894a5c5d
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-22 13:02:56 +0200

    add sample test data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,8 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlDialectInspectionForFile
+
+INSERT INTO inventory_items (id, label, quantity) VALUES ('1', 'Blue whiteboard markers', 4),
+                                                         ('2', 'Green whiteboard markers', 6),
+                                                         ('3', 'Orange whiteboard markers', 6),
+                                                         ('4', 'Red whiteboard markers', 6),
+                                                         ('5', 'Oatly litres', 5);
