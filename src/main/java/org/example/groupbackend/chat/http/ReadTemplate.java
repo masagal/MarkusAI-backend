8f7e4d61fd1e8cd0cf1c636adb4a644578767085
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-25 11:28:21 +0200

    add template and dto for chat gpt to give appropriate responses
    Co-authored-by: Ludvig Fendert <ludvig.fendert@appliedtechnology.se>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>
    Co-authored-by: Simon Kendall <simon.a.kendall@gmail.com>

---
+++ src/main/java/org/example/groupbackend/chat/http/ReadTemplate.java
@@ -0,0 +1,25 @@
+package org.example.groupbackend.chat.http;
+
+import org.example.groupbackend.request.RequestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadTemplate {
+    private static final Logger logger = LoggerFactory.getLogger(RequestController.class);
+
+    public static List<String> readTemplate() {
+        Path path = Paths.get("src/main/resources/template");
+        logger.info("Path: {}", path);
+        try {
+            return Files.readAllLines(path);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+}
