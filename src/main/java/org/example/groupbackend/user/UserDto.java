1a5d5d13d0f89abc444b8db083af0a35631d0b1e
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-25 17:22:08 +0200

    refactor user dto for consistency

--- src/main/java/org/example/groupbackend/user/UserDto.java
+++ src/main/java/org/example/groupbackend/user/UserDto.java
@@ -1,40 +1,7 @@
 package org.example.groupbackend.user;
 
-public class UserDto {
-    private Long id;
-    private String name;
-    private String email;
-    private Boolean isAdmin;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Boolean getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setIsAdmin(Boolean isAdmin) {
-        this.isAdmin = isAdmin;
+public record UserDto(Long id, String clerkId, String name, String email, boolean isAdmin) {
+    static public UserDto fromUser(User user) {
+        return new UserDto(user.getId(), user.getClerkId(), user.getName(), user.getEmail(), user.getIsAdmin());
     }
 }
