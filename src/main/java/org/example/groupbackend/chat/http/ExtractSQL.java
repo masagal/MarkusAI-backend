db4cb43ecc8e221bdedae8304ee7202b1e0ba8dc
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-26 09:37:09 +0200

    implement instructions for chat to be more dynamic when getting requests from users
    Co-authored-by: Ludvig Fendert <ludvig.fendert@appliedtechnology.se>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>
    Co-authored-by: Simon Kendall <simon.a.kendall@gmail.com>

--- src/main/java/org/example/groupbackend/chat/http/ExtractSQL.java
+++ src/main/java/org/example/groupbackend/chat/http/ExtractSQL.java
@@ -1,4 +1,7 @@
 package org.example.groupbackend.chat.http;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public record ExtractSQL(String sqlStatement) {
 }
