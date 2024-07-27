797adddd03fea719db977a6f0b4bc62e6237d573
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-27 10:41:58 +0200

    add test for non admin user changing order status

--- src/test/java/org/example/groupbackend/orderMockApi/OrderControllerTest.java
+++ src/test/java/org/example/groupbackend/orderMockApi/OrderControllerTest.java
@@ -1,42 +1,22 @@
 package org.example.groupbackend.orderMockApi;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.example.groupbackend.filterRequests.FilterRequests;
-import org.example.groupbackend.products.Product;
-import org.example.groupbackend.products.ProductDbRepo;
-import org.example.groupbackend.user.User;
-import org.example.groupbackend.user.UserRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Order;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
@@ -51,8 +31,6 @@ public class OrderControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private OrderRepository orderRepository;
-    @Autowired
-    private UserRepository userRepository;
 
     @MockBean
     HttpServletRequest request;
@@ -86,14 +64,14 @@ public class OrderControllerTest {
     @Order(2)
     public void shouldChangeStatusOfSecondOrderToARRIVED() throws Exception {
         String url = "http://localhost:%s/orders".formatted(port);
-        String newOrder = """
+        String changeOrder = """
                    {
                   "orderId": 2,
                   "orderStatus": "ARRIVED"
                    }
                 """;
 
-        mockMvc.perform(patch(url).header(HttpHeaders.AUTHORIZATION, token).contentType(MediaType.APPLICATION_JSON).content(newOrder))
+        mockMvc.perform(patch(url).header(HttpHeaders.AUTHORIZATION, token).contentType(MediaType.APPLICATION_JSON).content(changeOrder))
                 .andDo(print()) // print response details
                 .andExpect(status().isOk());
 
@@ -120,5 +98,28 @@ public class OrderControllerTest {
 
         assertThat(this.orderRepository.findAll()).hasSize(3);
         assertThat(this.orderRepository.findById(3L)).isNotNull();
+    }
+
+    @Test
+    @Order(4)
+    public void nonAdminUserCannotChangeStatusToOrder() throws Exception {
+        String url = "http://localhost:%s/orders".formatted(port);
+        String changeOrder = """
+                   {
+                  "orderId": 1,
+                  "orderStatus": "ARRIVED"
+                   }
+                """;
+
+        String fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyXzJqakl6R3E4UE82bW" +
+                "tSYUY4YVZhYnA0T1ZYUiIsIm5hbWUiOiJOb25BZG1pbiBtYXRoIiwiZW1haWwiOiJhbHdheXN0aGVyZUBn" +
+                "bWFpbC5jb20ifQ.q-_0qKSYUmobjy58TLu3yQ2ul2emzGWtSkb6u3PkoI8";
+
+        mockMvc.perform(patch(url).header(HttpHeaders.AUTHORIZATION, fakeToken).contentType(MediaType.APPLICATION_JSON).content(changeOrder))
+                .andDo(print()) // print response details
+                .andExpect(status().isForbidden());
+
+        assertThat(this.orderRepository.findAll().get(0).getStatus()).isEqualTo(OrderStatus.PENDING);
+
     }
 }
