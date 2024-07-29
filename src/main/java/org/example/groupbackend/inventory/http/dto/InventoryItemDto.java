bb453766b6b22f3dc0899e81486e2cdb85ac9f70
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-29 16:19:15 +0200

    inform inventory items about their physical location

--- src/main/java/org/example/groupbackend/inventory/http/dto/InventoryItemDto.java
+++ src/main/java/org/example/groupbackend/inventory/http/dto/InventoryItemDto.java
@@ -2,8 +2,8 @@ package org.example.groupbackend.inventory.http.dto;
 
 import org.example.groupbackend.inventory.model.InventoryItem;
 
-public record InventoryItemDto(String id, String name, String quantity) {
+public record InventoryItemDto(String id, String name, String quantity, String location) {
     public static InventoryItemDto getDto(InventoryItem item) {
-        return new InventoryItemDto(item.getId(), item.getLabel(), item.getQuantity().toString());
+        return new InventoryItemDto(item.getId(), item.getLabel(), item.getQuantity().toString(), item.getLocation());
     }
 }
