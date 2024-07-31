9843389c3718f9faae68f01daddda9b2198d6618
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-31 16:18:48 +0200

    update name in dto
    Co-authored-by: Ludvig Fendert <ludvig.fendert@appliedtechnology.se>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>

--- src/main/java/tech/masagal/markusai/user/UserDto.java
+++ src/main/java/tech/masagal/markusai/user/UserDto.java
@@ -1,6 +1,6 @@
 package tech.masagal.markusai.user;
 
-public record UserDto(Long id, String clerkId, String name, String email, boolean isAdmin, String imageUrl, String invitationUrl) {
+public record UserDto(Long id, String clerkId, String name, String email, boolean isAdmin, String imageUrl, String invitationToken) {
     static public UserDto fromUser(User user) {
         return new UserDto(user.getId(), user.getClerkId(), user.getName(), user.getEmail(), user.getIsAdmin(), user.getImageUrl(), user.getInvitationToken());
     }
